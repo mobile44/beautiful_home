250b64012daa6c11d9f79b714d5f470044c56161
mobile44 <albertwong.wk@gmail.com>
2021-12-26 14:46:30 -0800

    add: change back to browserrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 import './App.css';
 import Home from "./Home";
 import Album from "./Album";
@@ -11,10 +11,14 @@ function App(props) {
   const [activeHome, setActiveHome] = useState(false);
   const [activeAlbum, setActiveAlbum] = useState(false);
   const [activeContact, setActiveContact] = useState(false);
-
+  
   const toggleNav = () => {
     setToggleMenu(!toggleMenu);
   }
+  console.log("Path: ", window.location.pathname);
+  let windowPath = window.location.pathname;
+  windowPath = windowPath.substring(16,windowPath.length);
+  console.log("Pathname:", windowPath);
 
   useEffect(() => { //initiate && reload update
     try {
@@ -76,26 +80,26 @@ function App(props) {
 
   return (
     <Router>
-    <nav>
-      {(toggleMenu || screenWidth > 500) && (
-        <div className="list">
-          <Link className={activeHome?"aitems":"items"} to="/" onClick={()=>updatePage("Home")}>Home</Link>
-          <Link className={activeAlbum?"aitems":"items"} to="/album" onClick={()=>updatePage("Album")}>Album</Link>
-          <Link className={activeContact?"aitems":"items"} to="/contact" onClick={()=>updatePage("Contact")}>Contact</Link>
+      <nav>
+        {(toggleMenu || screenWidth > 500) && (
+          <div className="list">
+            <Link className={activeHome?"aitems":"items"} to="/beautiful_home/" onClick={()=>updatePage("Home")}>Home</Link>
+            <Link className={activeAlbum?"aitems":"items"} to="/beautiful_home/album" onClick={()=>updatePage("Album")}>Album</Link>
+            <Link className={activeContact?"aitems":"items"} to="/beautiful_home/contact" onClick={()=>updatePage("Contact")}>Contact</Link>
+          </div>
+        )}
+        {/*<button onClick={toggleNav} className="btn">Menu</button>*/}
+        <div className="shortMenu" onClick={toggleNav}>
+          <div className="bar1"></div>
+          <div className="bar2"></div>
+          <div className="bar3"></div>
         </div>
-      )}
-      {/*<button onClick={toggleNav} className="btn">Menu</button>*/}
-      <div className="shortMenu" onClick={toggleNav}>
-        <div className="bar1"></div>
-        <div className="bar2"></div>
-        <div className="bar3"></div>
-      </div>
-    </nav>
+      </nav>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/beautiful_home" element={<Home/>} />
-        <Route path="/album" element={<Album/>} />
-        <Route path="/contact" element={<Contact/>} />
+        <Route path="/beautiful_home/" element={<Home/>} />
+        <Route path="/beautiful_home/album" element={<Album/>} />
+        <Route path="/beautiful_home/contact" element={<Contact/>} />
       </Routes>
     </Router>
   )
