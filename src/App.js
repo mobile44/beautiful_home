25cf64a286ca254d70929b00aca8efd0f791b9a1
mobile44 <albertwong.wk@gmail.com>
2021-12-26 15:55:55 -0800

    add: change to hashrouter to address github refresh issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Link} from "react-router-dom";
 import './App.css';
 import Home from "./Home";
 import Album from "./Album";
